24766b213aeb61d0a5c723b6ea942ef315a4628e
Ryan Zahm <zahmryan@msu.edu>
2015-03-16 00:50:35 -0400

    helper methods completed, all tests pass

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 
 gem 'sinatra', '~> 1.4.5'
 gem 'rack', '1.5.2'
--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -4,6 +4,7 @@ module MethodsPracticeHelpers
     # Generate an image tag with the given source and the classes
     # "img-responsive" and "img-thumbnail". The image should also have an alt
     # attribute with the contents of alternate_text.
+    "<img class=\"img-responsive img-thumbnail\" src=\"#{source}\" alt=\"#{alternate_text}\">"
   end
 
   def bootstrap_alert(message, type='info')
@@ -11,12 +12,17 @@ module MethodsPracticeHelpers
     # to remember all HTML needed to do it right). It should show the message in
     # the "message" variable and should have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
+    "<div class='alert alert-#{type} alert-dismissible' role='alert'>
+      <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+      #{message}
+    </div>"
   end
 
   def current_date_and_time
     # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
     # Time.now.strftime is a great method chain you can use for this purpose.
     # See how to use it at: http://apidock.com/ruby/Time/strftime
+    Time.now.strftime("%B %-d, %Y at %-I:%M%P")
   end
 
   # ------------------------------------------
